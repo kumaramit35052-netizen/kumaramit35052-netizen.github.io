8083939a0937220596908b0ef4dfcd5e3da7664e
kumaramit35052-netizen <kumaramit35052@gmail.com>
2026-03-07 19:45:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
-/* ================= NAVBAR MOBILE AUTO CLOSE ================= */
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll('.nav-link, .dropdown-item').forEach(item => {
-    item.addEventListener('click', () => {
-      if (window.innerWidth < 992) {
-        const menu = document.querySelector('.navbar-collapse');
-        if (menu && menu.classList.contains('show')) {
-          menu.classList.remove('show');
-        }
-      }
-    });
-  });
+const img = document.querySelector(".profile-img");
+if(img){
+document.addEventListener("mousemove",(e)=>{
+let x = (window.innerWidth/2 - e.pageX)/35;
+let y = (window.innerHeight/2 - e.pageY)/35;
+img.style.transform = `rotateY(${x}deg) rotateX(${y}deg) scale(1.05)`;
 });
-// NAVBAR SCROLL EFFECT
-window.addEventListener("scroll", () => {
-  const nav = document.querySelector(".navbar");
-  if(window.scrollY > 50){
-    nav.classList.add("scrolled");
-  }else{
-    nav.classList.remove("scrolled");
-  }
-});
-// Scroll glass effect
-window.addEventListener("scroll",()=>{
-  const nav=document.querySelector(".navbar");
-  if(window.scrollY>60){
-    nav.classList.add("nav-scrolled");
-  }else{
-    nav.classList.remove("nav-scrolled");
-  }
-});
+}
